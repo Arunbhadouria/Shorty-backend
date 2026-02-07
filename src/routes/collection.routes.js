1723591291df99c6be4f8ab9f5be46d20e86d0db
Atomic_Arun <169286113+Arunbhadouria@users.noreply.github.com>
2026-02-07 16:36:18 +0530

    Add collection routes with authentication middleware

--- src/routes/collection.routes.js
+++ src/routes/collection.routes.js
@@ -1,1 +1,15 @@
+import express from "express";
+import { protect } from "../middlewares/auth.middleware.js";
+import {
+  createCollection,
+  getCollections,
+} from "../controllers/collection.controller.js";
 
+const router = express.Router();
+
+router.use(protect);
+
+router.post("/", createCollection);
+router.get("/", getCollections);
+
+export default router;
